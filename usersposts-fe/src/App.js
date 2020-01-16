57d8e87663e6c81246f26db8e9b719080daa710f
Hui <jeffrey.x.hui@gmail.com>
2020-01-15 18:08:52 -0800

    gets for both posts and users connected on front end

--- usersposts-fe/src/App.js
+++ usersposts-fe/src/App.js
@@ -11,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <Route path='/users' component={Users} />
+      <Route path='/posts' component={Posts} />
     </div>
   );
 }
--- usersposts-fe/src/components/Posts.js
+++ usersposts-fe/src/components/Posts.js
@@ -1,1 +1,46 @@
-import React from 'react';
+//react
+import React, { useEffect } from 'react';
+//redux
+import { connect, useDispatch } from 'react-redux'
+import constants from '../redux/constants'
+//axios
+import Axios from 'axios'
+
+
+const Posts = props => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    Axios.get('http://localhost:5000/api/posts')
+      .then( res => {
+        console.log(res)
+        dispatch({ type: constants.GET_POSTS, payload: res.data.posts })
+      })
+      .catch( err => {
+        console.log(err);
+      })
+  }, [])
+
+  return (
+    <div className='posts__cont'>
+      {
+        props.posts
+          ?
+            props.posts.map((post, index) => (
+              <div key={index} className='posts__card'>
+                <p>{post.text}</p>
+              </div>
+            ))
+          :
+            null
+      }
+    </div>
+  )
+}
+
+const mapStateToProps = ({ postReducer }) => {
+  return {
+    posts: postReducer.posts
+  }
+}
+
+export default connect(mapStateToProps)(Posts);
